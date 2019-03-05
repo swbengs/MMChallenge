58e3856465468e7032b3d415244f9c266c4cf68b
swbengs <swbengs@yahoo.com>
2019-03-05 08:37:52 -0600

    Fixed classname mixup for trainer. Added implementation for getTrainerByToken

--- myfirstsite/app/TrainerRepository.php
+++ myfirstsite/app/TrainerRepository.php
@@ -8,10 +8,14 @@ use App\User;
 Class to abstract the database representation of User aka our Trainer. Allows the controllers to not worry about how it's actually stored.
 */
 
-class PokemonRepository
+class TrainerRepository
 {
-    public function getUserByToken($api_token)
+    //gives trainer id and email
+    public function getTrainerByToken($api_token)
     {
         $user = User::where('api_token', $api_token)->get();
+        $result = array(['trainer_id' => $user->id, 'name' => $user->email]);
+
+        return $result;
     }
 }
